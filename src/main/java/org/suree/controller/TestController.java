451b98f2e7eb8865458be8de3b874b47a9cb2ddd
Sure <edding_sure@foxmail.com>
2016-11-13 21:59:03 +0800

    mysql go

--- src/main/java/org/suree/controller/TestController.java
+++ src/main/java/org/suree/controller/TestController.java
@@ -1,8 +1,12 @@
 package org.suree.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.suree.domain.TaiKyokuTable;
+import org.suree.mappers.TaiKyokuTableMapper;
 import org.suree.model.Request;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,12 +21,18 @@ import java.util.Map;
 @RequestMapping(value = "/test")
 public class TestController {
 
+    @Resource
+    private TaiKyokuTableMapper taiKyokuTableMapper;
+
     @RequestMapping(value = "/api/{convId}", method = RequestMethod.GET)
     public Map<String, Object> testApi(@RequestParam(name = "name", required = true)String name,
                                        @PathVariable(value = "convId")Integer convId) {
         Map results = new HashMap();
-        results.put("name", name);
-        results.put("convId", convId);
+
+        TaiKyokuTable taiKyokuTable = taiKyokuTableMapper.selectByPrimaryKey(1);
+
+        results.put("results", taiKyokuTable);
+
         return results;
     }
 
